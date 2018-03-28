1e19c62be0adbe6f4dfec440f285736853c5aa66
Saugat Tripathi <saugat.tripathi@bison.howard.edu>
2018-03-27 21:26:57 -0400

    Added some statistics for each column

--- script.py
+++ script.py
@@ -13,6 +13,78 @@ df_iris=pd.read_csv('iris.csv')
 
 # test if the file is imported correctly
 print df_iris.head(10)
-print 'For Sepal Length'
+# saving each numercial column as pandas series 
+speal_length =  df_iris['Sepal Length']
+speal_width =  df_iris['Sepal Width']
+petal_length =  df_iris['Petal Length']
+petal_width =  df_iris['Petal Width']
+
+
+print " *******************************************************************************"
+print 'For Sepal Length :- '
+print 'Count :', speal_length.count()	
+print 'Minimum :'	, speal_length.min()
+print 'Maximum : ', speal_length.max()
+print 'Mean : ', speal_length.mean()	
+print 'Median : ', speal_length.median()
+print 'Mode : ', speal_length.mode()
+print 'Quartile : ' 
+print 'Range : '	
+print 'Variance : '	
+print 'Standard Deviation : '	
+print 'Coefficient of Variation : '	
+print 'Skewness : '	
+print 'Kurtosis : '
+print " *******************************************************************************"
+
+
+print 'For Sepal Width :- '
+print 'Count :'	
+print 'Minimum :'	
+print 'Maximum : '
+print 'Mean : '	
+print 'Median : '
+print 'Mode : '	
+print 'Quartile : ' 
+print 'Range : '	
+print 'Variance : '	
+print 'Standard Deviation : '	
+print 'Coefficient of Variation : '	
+print 'Skewness : '	
+print 'Kurtosis : '
+print " *******************************************************************************"
+
+
+print 'For Petal Length:- '
+print 'Count :'	
+print 'Minimum :'	
+print 'Maximum : '
+print 'Mean : '	
+print 'Median : '
+print 'Mode : '	
+print 'Quartile : ' 
+print 'Range : '	
+print 'Variance : '	
+print 'Standard Deviation : '	
+print 'Coefficient of Variation : '	
+print 'Skewness : '	
+print 'Kurtosis : '
+print " *******************************************************************************"
+
+print 'For Petal Width :- '
+print 'Count :'	
+print 'Minimum :'	
+print 'Maximum : '
+print 'Mean : '	
+print 'Median : '
+print 'Mode : '	
+print 'Quartile : ' 
+print 'Range : '	
+print 'Variance : '	
+print 'Standard Deviation : '	
+print 'Coefficient of Variation : '	
+print 'Skewness : '	
+print 'Kurtosis : '
+print " *******************************************************************************"
 
 
